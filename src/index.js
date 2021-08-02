d8584304707e4a6d95f75511fd943cf554e2696e
tomw66 <tom.wilson96@hotmail.com>
2021-08-02 15:56:33 +0100

    further build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-let myProject = [];
+import './style.css';
+
+const projectGroup = (() => {
+    class Project {
+        constructor (name) {
+            this.name = name;
+            this.list = [];
+        }
+    }
+
+    const createProject = (name) => {
+        let newProject = new Project(name);
+        return {newProject}
+    }
+
+    const deleteProject = () => {
+
+    }
+ return {
+     Project,
+     createProject,
+     deleteProject,
+ }
+})();
 
 const taskGroup = (() => {
     class Task {
@@ -23,8 +46,28 @@ const taskGroup = (() => {
 
     }
  return {
+     Task,
      createTask,
      editTask,
      deleteTask,
  }
 })();
+
+const manager = (() => {
+    const content = document.querySelector('#projectList');
+    const addProjectButton = content.querySelector('#addProject');
+
+    const addProject = () => {
+        let name = prompt('Project Name');
+        let newProject = new projectGroup.Project(name);
+        const projectButton = document.createElement('button');
+        projectButton.textContent = name;
+        projectList.appendChild(projectButton) 
+    }
+    addProjectButton.addEventListener('click', addProject);
+
+})();
+let a = new projectGroup.Project('smeggy');
+a.list.push('yes');
+
+console.log(a, a.name)
